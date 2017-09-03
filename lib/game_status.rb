fbad8828f09fcb8572994a33ce76172118b56e6e
Emily Morgan <emorgan05@gmail.com>
2017-09-03 21:28:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,38 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_array|
+    if win_array.all? { |win_index| position_taken?(board, win_index) }
+      win_index_1 = win_array[0]
+      win_index_2 = win_array[1]
+      win_index_3 = win_array[2]
+
+      if board[win_index_1] == board[win_index_2] && board[win_index_1] == board[win_index_3]
+        return win_array
+      else
+        return false
+      end
+    end
+  end
+end
+
+def full?(board)
+  empty_position = board.detect { " " }
+  if empty_position
+    return false
+  else
+    return true
+  end
+end
